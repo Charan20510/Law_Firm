2ab558408fbe24321a54b0467d43acf955f7d2a0
Charan20510 <avvarucharantej11@gmail.com>
2026-03-27 17:13:20 +0530

    Add auto superuser creation for Render deployment

---
+++ core/management/commands/create_superuser.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = 'Create a superuser if it does not already exist'
+
+    def handle(self, *args, **options):
+        username = 'admin'
+        email = 'admin@example.com'
+        password = 'ChangeMe123'  # Change this!
+        
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(
+                self.style.WARNING(f'Superuser {username} already exists, skipping creation')
+            )
+        else:
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(
+                self.style.SUCCESS(f'Successfully created superuser {username}')
+            )
